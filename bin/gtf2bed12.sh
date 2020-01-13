02c8f0804d6dd23b049df5a395f4aa2d459e1002
kingzhuky <kingzhuky88@gmail.com>
2020-01-13 13:19:18 +0800

    Modify the function of Motif Searching

--- bin/gtf2bed12.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#$1 argv 1 : gtf file, the name of output is genes.bed
-gtf_file=$1
-gtfToGenePred -genePredExt -geneNameAsName2 ${gtf_file} genes.tmp
-genePredToBed genes.tmp ${gtf_file/.gtf/.bed}
-rm genes.tmp
