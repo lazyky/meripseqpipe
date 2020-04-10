edaf9a9be08e445caa4931fdd8fdd2e0e34f0159
kingzhuky <zhuky@sysucc.org.cn>
2020-04-10 20:42:25 +0800

    modify function of reading designfile

--- lib/LikeletUtils.groovy
+++ lib/LikeletUtils.groovy
@@ -26,38 +26,66 @@ class LikeletUtils {
 
         // Check if a row has the expected number of item, adjusted from Sarek 
           static def checkNumberOfItem(row, number) {
-            if (row.size() != number) exit 1, "Malformed row in TSV file: ${row}, see --help for more information"
+            if (row.size() != number) exit 1, println("Malformed row in TSV file: ${row}, see --help for more information")
             return true
           } 
 
         // Return status [0,1]
           // 0 == Normal, 1 == Tumor
           static def returnStatus(it) {
-            if (!(it in [0, 1])) exit 1, "Status is not recognized in TSV file: ${it}, see --help for more information"
+            if (!(it in [0, 1])) exit 1, println("Status is not recognized in TSV file: ${it}, see --help for more information")
             return it
           }
 
           // Return file if it exists
           static def returnFile(it) {
-            if (!file(it).exists()) exit 1, "Missing file in TSV file: ${it}, see --help for more information"
+            if (!file(it).exists()) exit 1, println("Missing file in TSV file: ${it}, see --help for more information")
             return file(it)
           }
         // extrct fastq information from tsvFile
-        static def extractFastq(tsvFile) {
+        static def extractFastq1(tsvFile) {
           // Channeling the TSV file containing FASTQ.
           // Format is: "subject gender status sample lane fastq1 fastq2"
-          Channel.from(tsvFile)
-          .splitCsv(sep: '\t')
+          def inputData = Channel.from(tsvFile)
+          .splitCsv(sep: '\t', skip: 1)
           .map { row ->
             LikeletUtils.checkNumberOfItem(row, 6)
-            def idPatient  = row[0]
-            def gender     = row[1]
-            def status     = LikeletUtils.returnStatus(row[2].toInteger()) // get normal or tumor status 
-            def idSample   = row[3]
-            def fastqFile1 = LikeletUtils.returnFile(row[4])
-            def fastqFile2 = LikeletUtils.returnFile(row[5])
-            [idPatient, gender, status, idSample, fastqFile1, fastqFile2]
+            def idSample  = row[0]
+            def fastqFile1 = file(row[1])
+            def fastqFile2 = file(row[2])
+            def gzip = false
+            def readsSingle = false
+            if (row[1].endsWith(".gz") == true ){
+              gzip = true
+            } 
+            println (row[2])
+            if (row[2].endsWith("false") == true){
+              readsSingle = true
+              [idSample, [fastqFile1], readsSingle, gzip]
+            } else {
+              [idSample, [fastqFile1, fastqFile2], readsSingle, gzip]
+            }
           }
+          def ipData = Channel.from(tsvFile)
+          .splitCsv(sep: '\t', skip: 1)
+          .map { row ->
+            LikeletUtils.checkNumberOfItem(row, 6)
+            def idSample  = row[0]
+            def fastqFile1 = file(row[3])
+            def fastqFile2 = file(row[4])
+            def gzip = false
+            def readsSingle = false
+            if (row[3].endsWith(".gz") == true ){
+              gzip = true
+            } 
+            if (row[4].endsWith("false") == true){
+              readsSingle = true
+              [idSample, [fastqFile1], readsSingle, gzip]
+            } else {
+              [idSample, [fastqFile1, fastqFile2], readsSingle, gzip]
+            }          
+          }
+          return inputData.mix(ipData)
         }
 
 
