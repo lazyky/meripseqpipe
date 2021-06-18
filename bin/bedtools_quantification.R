0ca6b22b80e0c414d6c64a44a0fd8e82c695672e
juneb4869 <69783852+juneb4869@users.noreply.github.com>
2021-06-18 17:04:30 +0800

    Update bedtools_quantification.R
    change the rpkm method

--- bin/bedtools_quantification.R
+++ bin/bedtools_quantification.R
@@ -10,8 +10,8 @@ bam_stat_table <- read.table(bam_stat_summary,row.names = 1)
 filelist =list.files(path = "./",pattern = ".count")
 ## Generate the quantificative value of peaks referred to RPKM
 rpkm_peaks_list <- NULL
-rpkm_peaks_list1 <- NULL
-rpkm_peaks_list2 <- NULL
+#rpkm_peaks_list1 <- NULL
+#rpkm_peaks_list2 <- NULL
 for(sample_id in designtable$Sample_ID){
   input_count_file <- grep(paste0("[.]",sample_id,"[.]input"),filelist,value = TRUE)
   input_count_table <- read.table(file = input_count_file, sep = "\t", row.names = NULL,header = T)
@@ -23,19 +23,19 @@ for(sample_id in designtable$Sample_ID){
   bam_stat_index = grep(paste0("^",sample_id,"[.]ip"),rownames(bam_stat_table))
   ip_rpkm =  apply(ip_count_table,1,function(x) (as.numeric(x[5])/(as.numeric(x[3])-as.numeric(x[2]))*1000/bam_stat_table[bam_stat_index,]*1000000))
   
-  rpkm <- as.matrix((ip_rpkm)/(input_rpkm+ip_rpkm))
-  rpkm1<- as.matrix((ip_rpkm)/(input_rpkm+1))
-  rpkm2<- as.matrix((ip_rpkm+1)/(input_rpkm+1))
+  rpkm <- as.matrix((ip_rpkm+1)/(input_rpkm+1))
+  #rpkm1<- as.matrix((ip_rpkm)/(input_rpkm+1))
+  #rpkm2<- as.matrix((ip_rpkm)/(input_rpkm+ip_rpkm))
   colnames(rpkm)[1] <- sample_id
-  colnames(rpkm1)[1] <- sample_id
-  colnames(rpkm2)[1] <- sample_id
+  #colnames(rpkm1)[1] <- sample_id
+  #colnames(rpkm2)[1] <- sample_id
   rpkm_peaks_list <- cbind(rpkm_peaks_list,rpkm)
-  rpkm_peaks_list1 <- cbind(rpkm_peaks_list1,rpkm1)
-  rpkm_peaks_list2 <- cbind(rpkm_peaks_list2,rpkm2)	
+  #rpkm_peaks_list1 <- cbind(rpkm_peaks_list1,rpkm1)
+  #rpkm_peaks_list2 <- cbind(rpkm_peaks_list2,rpkm2)	
 }
 rownames(rpkm_peaks_list) <- input_count_table[,4] 
-rownames(rpkm_peaks_list1) <- input_count_table[,4] 
-rownames(rpkm_peaks_list2) <- input_count_table[,4] 
+#rownames(rpkm_peaks_list1) <- input_count_table[,4] 
+#rownames(rpkm_peaks_list2) <- input_count_table[,4] 
 write.table(rpkm_peaks_list,sep = "\t",file = "bedtools_quantification.matrix",quote = F)
-write.table(rpkm_peaks_list1,sep = "\t",file = "bedtools_quantification.inputadd.matrix",quote = F)
-write.table(rpkm_peaks_list2,sep = "\t",file = "bedtools_quantification.alladd.matrix",quote = F)
+#write.table(rpkm_peaks_list1,sep = "\t",file = "bedtools_quantification.inputadd.matrix",quote = F)
+#write.table(rpkm_peaks_list2,sep = "\t",file = "bedtools_quantification.alladd.matrix",quote = F)
